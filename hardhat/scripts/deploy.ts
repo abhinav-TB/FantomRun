b146af1552a97fcc74531bfa901d46cc060ad35a
UltimateRoman <59477486+UltimateRoman@users.noreply.github.com>
2023-03-04 23:24:46 +0530

    Updated deploy script

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,18 +1,19 @@
 import { ethers } from "hardhat";
 
+const stakingTokenAddress = "";
+const rewardTokenAddress = "";
+const NFTcontractAddress = "";
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const TokenStaker = await ethers.getContractFactory("TokenStaker");
+  const tokenStaker = await TokenStaker.deploy(
+    stakingTokenAddress,
+    rewardTokenAddress,
+    NFTcontractAddress
+  );
+  await tokenStaker.deployed();
 
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log(`Staking contract deployed to ${tokenStaker.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
